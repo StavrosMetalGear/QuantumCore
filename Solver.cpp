0c647299800fb71aca5e533840bee3b708d141cc
Stavros Zymvragoudakis <stavrosiu@hotmail.com>
2025-05-31 11:42:06 +0300

    Add project files.

---
+++ Solver.cpp
@@ -0,0 +1,12 @@
+﻿#include "Solver.h"
+#include <cmath>
+#include "pch.h"
+
+
+
+// Very simple model: E = (π² ℏ²) / (2mL²)
+double SolveSchrodinger(double mass, double length, double potential) {
+    const double hbar = 1.0545718e-34;
+    double energy = (std::pow(3.14159, 2) * std::pow(hbar, 2)) / (2 * mass * std::pow(length, 2));
+    return energy + potential; // simplified example
+}
---
+++ Solver.h
@@ -0,0 +1,4 @@
+#pragma once
+
+extern "C" __declspec(dllexport)
+double SolveSchrodinger(double mass, double length, double potential);
